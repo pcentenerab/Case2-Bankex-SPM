35ebaa33a618cd85d6f3c316831f15327a1f75d2
Patricia Centenera Béjar <patriciacentenerabejar@gmail.com>
2020-04-20 11:16:12 +0200

    Initial Commit

---
+++ Case2-Bankex-SPM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Case2-Bankex-SPM
+//
+//  Created by Belén on 20/04/2020.
+//  Copyright © 2020 IWEB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
